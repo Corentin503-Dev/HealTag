cb465e8b4ff0f65e7d3db9cb5aebac11a3d7c9ff
Corentin503 <82731581+Corentin503-Dev@users.noreply.github.com>
2023-07-04 17:35:27 +0200

    Update Main.php
    Patch $event->getPlayer() to $event->getEntity()
    Update function getHealtBar with a str_repeat

--- src/Corentin503HTB/Main.php
+++ src/Corentin503HTB/Main.php
@@ -22,80 +22,13 @@ class Main extends PluginBase implements Listener
 
     public function onDamage(EntityDamageEvent $event)
     {
-        if ($event->getEntity() instanceof Player) $event->getEntity()->setScoreTag($this->getHealthBar($event->getPlayer()->getHealth()));
+        if ($event->getEntity() instanceof Player) $event->getEntity()->setScoreTag($this->getHealthBar($event->getEntity()->getHealth()));
     }
 
     public function getHealthBar(int|float $heal): string
     {
-        $string = "";
-
         $heal = (int)$heal;
-
-        switch ($heal) {
-            case 0:
-                $string = "§c||||||||||||||||||||";
-                break;
-            case 1:
-                $string = "§a|§c|||||||||||||||||||";
-                break;
-            case 2:
-                $string = "§a||§c||||||||||||||||||";
-                break;
-            case 3:
-                $string = "§a|||§c|||||||||||||||||";
-                break;
-            case 4:
-                $string = "§a||||§c||||||||||||||||";
-                break;
-            case 5:
-                $string = "§a|||||§c|||||||||||||||";
-                break;
-            case 6:
-                $string = "§a||||||§c||||||||||||||";
-                break;
-            case 7:
-                $string = "§a|||||||§c|||||||||||||";
-                break;
-            case 8:
-                $string = "§a||||||||§c||||||||||||";
-                break;
-            case 9:
-                $string = "§a|||||||||§c|||||||||||";
-                break;
-            case 10:
-                $string = "§a||||||||||§c||||||||||";
-                break;
-            case 11:
-                $string = "§a|||||||||||§c|||||||||";
-                break;
-            case 12:
-                $string = "§a||||||||||||§c||||||||";
-                break;
-            case 13:
-                $string = "§a|||||||||||||§c|||||||";
-                break;
-            case 14:
-                $string = "§a||||||||||||||§c||||||";
-                break;
-            case 15:
-                $string = "§a|||||||||||||||§c|||||";
-                break;
-            case 16:
-                $string = "§a||||||||||||||||§c||||";
-                break;
-            case 17:
-                $string = "§a|||||||||||||||||§c|||";
-                break;
-            case 18:
-                $string = "§a||||||||||||||||||§c||";
-                break;
-            case 19:
-                $string = "§a|||||||||||||||||||§c|";
-                break;
-            case 20:
-                $string = "§a||||||||||||||||||||§c";
-                break;
-        }
-        return $string;
+        $bar = "§a" . str_repeat("|", $heal) . "§c" . str_repeat("|", 20 - $heal);
+        return $bar;
     }
 }
